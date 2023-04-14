ade0b331aa45be58491aa764173df57cf09e84f4
Marius Borsu Lefevre <marius.borsu-lefevree@isae-supaero.fr>
2023-04-14 08:09:15 +0200

    part 1

---
+++ mysite/disks/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=200)
+
+class Album(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    
+class Track(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    composer = models.CharField(max_length=200)
+    milliseconds = models.IntegerField(default=0)
+    bytes = models.IntegerField(default=0)
+    unitPrice = models.FloatField(default=0.0)
